3520af171a76bacd212982a49f3a0f6835499cc7
keidarb <keidar11@gmail.com>
2021-02-23 14:55:13 +0200

    create docker-compose file

--- rest_app.py
+++ rest_app.py
@@ -65,5 +65,9 @@ def stop_server():
     except:
         return {'status': 'error', 'reason': "didn't manage to close rest app'"}, 500
 
+@app.route('/hello')
+def hello():
+    return {'status': 'ok', 'user added': 'keidar'}, 201  # status code
 
-app.run(host='127.0.0.1', debug=True, port=5000)
+
+app.run(host='0.0.0.0', debug=True)
